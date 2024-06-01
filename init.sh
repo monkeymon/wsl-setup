9f7f09889121a54393853ba744b193639b0c36c3
Chris Sim <monkeymon@users.noreply.github.com>
2024-06-01 15:26:58 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,19 +4,10 @@ ssh-keygen -t ed25519 -C "chris.sim+windows-wsl@antpolis.com" -f ~/.ssh/id_ed255
 
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install zsh software-properties-common
-
-
-
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-curl -fsSL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscli.zip
-
 sudo add-apt-repository ppa:ondrej/php
-sudo apt -y update
-sudo apt -y install unzip php8.3 php8.3-{mysql,zip,bcmath,dev,zip,common,cli,fpm,gd,mysql,curl,xml,mbstring,bcmath}
+zsh
 
-unzip awscli.zip
-sudo ./aws/install 
+
 
 
 # export NVM_DIR="$HOME/.nvm"
